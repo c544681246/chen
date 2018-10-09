a6a72b17acdcd2581944b8e9cbbf36202fe79748
tao <544681246@qq.com>
2018-10-09 08:27:03 +0800

    1.5网页静态化

--- pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -3,9 +3,13 @@ package com.pinyougou.page.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbGoodsDescMapper;
 import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
+import com.pinyougou.mapper.TbItemMapper;
 import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @Service
 public class ItemPageServiceImpl implements ItemPageService {
@@ -25,17 +30,37 @@ public class ItemPageServiceImpl implements ItemPageService {
     private TbGoodsMapper goodsMapper;
     @Autowired
     private TbGoodsDescMapper goodsDescMapper;
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
+    @Autowired
+    private TbItemMapper itemMapper;
     @Override
     public boolean genItemHtml(Long goodsId) {
         try {
             Configuration configuration = freeMarkerConfigurer.getConfiguration();
             Template template = configuration.getTemplate("item.ftl");
             Map map=new HashMap();
+            //商品表数据
             TbGoods goods = goodsMapper.selectByPrimaryKey(goodsId);
             map.put("goods",goods);
+            //商品扩展属性
             TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
             map.put("goodsDesc",goodsDesc);
-            
+            //商品分类
+            String itemCat1 = itemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+            String itemCat2 = itemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+            String itemCat3 = itemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+            map.put("itemCat1", itemCat1);
+            map.put("itemCat2", itemCat2);
+            map.put("itemCat3", itemCat3);
+            //4.SKU 列表
+            TbItemExample example=new TbItemExample();
+            TbItemExample.Criteria criteria = example.createCriteria();
+            criteria.andStatusEqualTo("1");//状态为有效
+            criteria.andGoodsIdEqualTo(goodsId);//指定 SPU ID
+            example.setOrderByClause("is_default desc");//按照状态降序，保证第一个为默认
+            List<TbItem> itemList = itemMapper.selectByExample(example);
+            map.put("itemList", itemList);
             Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(pagedir ,goodsId + ".html")),"UTF-8"));
             template.process(map,out);
             out.close();
